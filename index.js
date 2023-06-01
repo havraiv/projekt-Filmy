d4b6388af2ab533b49b1c771ec61e02596b21ddc
Iva Havranová <havranova.i@seznam.cz>
2023-06-01 20:01:00 +0200

    sort titles

--- index.js
+++ index.js
@@ -14,6 +14,17 @@ fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
   .then((response) => response.json())
   .then((data) => {
     movieArray = data;
-    console.log(movieArray);
-    showMovies(movieArray);
+    const sortTitles = movieArray.sort((a, b) => {
+      let fa = a.title.toLowerCase(),
+        fb = b.title.toLowerCase();
+
+      if (fa < fb) {
+        return -1;
+      }
+      if (fa > fb) {
+        return 1;
+      }
+      return 0;
+    });
+    showMovies(sortTitles);
   });
