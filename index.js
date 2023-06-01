4c6a9c447ebffcff9b21d2ff666a8e5e89576cdf
Iva Havranová <havranova.i@seznam.cz>
2023-06-01 18:45:03 +0200

    film list done

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 /* tady bude tvůj kód */
+import { Movie } from './Movie/index.js';
+
 console.log('JavaScript ve stránce funguje');
+
+let movieArray;
+const showMovies = (data) => {
+  data.forEach((film) => {
+    return Movie({ film });
+  });
+};
+
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/movie-api/movies')
+  .then((response) => response.json())
+  .then((data) => {
+    movieArray = data;
+    console.log(movieArray);
+    showMovies(movieArray);
+  });
