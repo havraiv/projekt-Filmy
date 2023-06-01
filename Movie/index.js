231437b564e57010ef13a31900ff7e7401ec1a3b
Iva Havranová <havranova.i@seznam.cz>
2023-06-01 20:29:50 +0200

    button creation in progress

--- Movie/index.js
+++ Movie/index.js
@@ -1,9 +1,10 @@
 const movieList = document.querySelector('#movies');
 export const Movie = ({ title, url, posterUrl, year, genres }) => {
-  return (movieList.innerHTML += `<div class="movie">
-  <img class="movie__img" src=${posterUrl} alt="${title}">
+  const movieElm = document.createElement('div');
+  movieElm.classList.add('movie');
+  movieElm.innerHTML = `<img class="movie__img" src=${posterUrl} alt="${title}">
   <h2 class="movie__title"><a href=${url}>${title}</a></h2>
   <p class="movie__year">${year}</p>
-  <p class="movie__genre">${genres.join(', ')}</p>
-</div>`);
+  <p class="movie__genre">${genres.join(', ')}</p>`;
+  return movieList.append(movieElm);
 };
